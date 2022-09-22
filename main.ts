2c87318c3536f26b7303b4de5df08a6c0dad0201
DAN-USB <114154395+DAN-USB@users.noreply.github.com>
2022-09-22 08:25:57 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showString("3")
+    basic.showString("2")
+    basic.showString("1")
+    basic.pause(100)
+})
+basic.forever(function () {
+	
+})
