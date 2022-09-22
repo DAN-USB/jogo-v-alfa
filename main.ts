f64040c373ae665729724c562c51723827f5aab5
DAN-USB <114154395+DAN-USB@users.noreply.github.com>
2022-09-22 08:50:36 -0300

    Atualizar 'pxt.json, main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,9 +1,26 @@
+let player_2 = 0
 input.onPinPressed(TouchPin.P0, function () {
     basic.showString("3")
     basic.showString("2")
     basic.showString("1")
-    basic.pause(100)
+    basic.showString("Ja!")
+    for (let index = 0; index < 20; index++) {
+        basic.pause(randint(1000, 8000))
+        basic.showIcon(IconNames.Target)
+        basic.pause(1000)
+        basic.clearScreen()
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    player_2 = 1
+    music.playTone(523, music.beat(BeatFraction.Whole))
+})
+input.onPinPressed(TouchPin.P2, function () {
+	
+})
+input.onPinPressed(TouchPin.P1, function () {
+    player_2 += 1
 })
 basic.forever(function () {
-	
+    music.playMelody("C5 B E C5 B E F A ", 120)
 })
